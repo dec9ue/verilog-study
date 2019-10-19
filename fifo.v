862f32c3a229421c6590187881fdd667d904afe0
dec9ue <decque@gmail.com>
2019-10-19 17:08:26 +0900

    added fifo function

---
+++ fifo.v
@@ -0,0 +1,74 @@
+module myfifo #(
+	parameter integer C_DATA_WIDTH = 64,
+	parameter integer C_FIFO_DEPTH = 10,
+	// before using this option, consider to extend FIFO_DEPTH
+	parameter integer C_USE_SIMUL_IO = 0
+)
+(
+	input  wire clk,
+	input  wire resetn,
+	output wire read_valid,
+	input  wire read_ready,
+	output wire [C_DATA_WIDTH-1:0] read_data,
+	input  wire write_valid,
+	output wire write_ready,
+	input  wire [C_DATA_WIDTH-1:0] write_data,
+	output wire full,
+	output wire empty
+);
+
+reg [C_DATA_WIDTH-1:0] data [0:C_FIFO_DEPTH-1];
+reg [$clog2(C_FIFO_DEPTH)-1:0] wp = 0;
+reg wp_wrapped = 0;
+reg [$clog2(C_FIFO_DEPTH)-1:0] rp = 0;
+reg rp_wrapped = 0;
+
+wire size =
+	( wp > rp ) ? wp - rp :
+	( wp < rp ) ? (wp+C_FIFO_DEPTH) - rp :
+	( wp_wrapped  != rp_wrapped ) ? C_FIFO_DEPTH : 0; 
+wire write_committed = write_ready && write_valid;
+wire read_committed  = read_ready && read_valid;
+
+assign full  = (wp == rp) && (wp_wrapped != rp_wrapped);
+assign empty = (wp == rp) && (wp_wrapped == rp_wrapped);
+// TODO is condtion (read_ready && write_valid) reasonable??
+// this condition can be eliminated but thruput downs
+assign read_valid =  (!empty);
+assign write_ready = (C_USE_SIMUL_IO) ?
+	(!full) || (read_ready && write_valid) :
+	(!full) ;
+assign read_data = data[rp];
+
+always @( posedge clk) begin
+	if(resetn == 0) begin
+		wp <= 0;
+		rp <= 0;
+		wp_wrapped <= 0;
+		rp_wrapped <= 0;
+	end
+	else
+	begin
+		if(write_committed) begin
+			data[wp] <= write_data;
+			if( wp < C_FIFO_DEPTH -1) begin
+				wp <= wp + 1;
+			end else begin
+				wp <= 0;
+				wp_wrapped = ~wp_wrapped;
+			end
+		end
+		if(read_committed) begin
+			if( rp < C_FIFO_DEPTH -1) begin
+				rp <= rp + 1;
+			end else begin
+				rp <= 0;
+				rp_wrapped = ~rp_wrapped;
+			end
+		end
+	end
+end
+
+
+endmodule
+
---
+++ fifo_test2.v
@@ -0,0 +1,132 @@
+
+module test_fifo #(
+	parameter integer C_DATA_WIDTH = 8,
+	parameter integer C_FIFO_DEPTH = 2
+) ();
+	reg clk = 0;
+	reg resetn = 0;
+	wire read_valid;
+	reg read_ready;
+	wire [C_DATA_WIDTH-1:0] read_data;
+	reg write_valid;
+	wire write_ready;
+	reg [C_DATA_WIDTH-1:0] write_data;
+	wire full;
+	wire empty;
+
+	wire write_committed = write_ready && write_valid;
+	wire read_committed  = read_ready && read_valid;
+
+	myfifo #(C_DATA_WIDTH,C_FIFO_DEPTH,0) fifo_dut(
+		.clk(clk),
+		.resetn(resetn),
+		.read_valid(read_valid),
+		.read_ready(read_ready),
+		.read_data(read_data),
+		.write_valid(write_valid),
+		.write_ready(write_ready),
+		.write_data(write_data),
+		.full(full),
+		.empty(empty)
+	);
+
+	reg disp_log = 0;
+	always begin
+		#5
+		clk <= ~clk;
+	end
+	always @ (posedge resetn) begin
+		$display("reset released");
+	end
+	always @ (posedge clk) begin
+		if(disp_log) begin
+		$display("===> clk");
+		if(empty) $display("empty");
+		if(full) $display("full");
+		if(write_committed) $display("write : %c",write_data);
+		if(read_committed) $display("read  : %c",read_data);
+		#1
+		$display("----");
+		end
+	end
+
+	initial begin
+		$display("fifo depth is %d(%dbit)",C_FIFO_DEPTH,$clog2(C_FIFO_DEPTH));
+	end
+
+	initial begin
+		#5
+		disp_log = 1;
+		@(posedge clk) resetn = 1;
+		#10
+		@(negedge clk) begin
+			read_ready = 1; // this read should not work
+			write_data = "a";
+			write_valid = 1;
+		end
+		#5
+		@(negedge clk) begin
+			read_ready = 0;
+			write_data = "b";
+			write_valid = 1;
+		end
+		#5
+		@(negedge clk) begin
+			read_ready = 0;
+			write_data = "c";
+			write_valid = 1;
+		end
+		#5
+		@(negedge clk) begin
+			read_ready = 1;
+			write_data = "d";
+			write_valid = 1;
+		end
+		#5
+		@(negedge clk) begin
+			read_ready = 1;
+			write_data = "e";
+			write_valid = 0;
+		end
+		#5
+		@(negedge clk) begin
+			read_ready = 1;
+			write_data = "f";
+			write_valid = 0;
+		end
+		#5
+		@(negedge clk) begin
+			read_ready = 1;
+			write_data = "0";
+			write_valid = 0;
+		end
+		#5
+		@(negedge clk) begin
+			read_ready = 0;
+			write_data = "a";
+			write_valid = 1;
+		end
+		#5
+		@(negedge clk) begin
+			read_ready = 1;
+			write_data = "b";
+			write_valid = 1;
+		end
+		#5
+		@(negedge clk) begin
+			read_ready = 1;
+			write_data = "c";
+			write_valid = 1;
+		end
+		#5
+		@(negedge clk) begin
+			read_ready = 1;
+			write_data = "0";
+			write_valid = 0;
+		end
+		#20
+		$finish;
+	end
+
+endmodule
+
